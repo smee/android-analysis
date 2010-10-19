02fc5a484d90d163352c3fe3e49bb79421866681
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2010-10-19 18:42:18 +0000

    results of action references between android apps, code, result and visualization
    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@367 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- src/main/clojure/android_manifest/scribble.clj
+++ src/main/clojure/android_manifest/scribble.clj
@@ -2,15 +2,17 @@
   (:use android-manifest.serialization
      [clojure.contrib.pprint :only (pprint)]))
 
+(defn get-actions [all-apps app-name]
+  (get-in all-apps [app-name :actions]))
+
 (defn filter-references [ref-map r]
   "Retain only apps that call a intent that is not defined within their own
    manifest.xml."
   (into {}
-    (filter (comp empty? vals)
-      (for [[action apps] ref-map]
-        (let [filtered-refs (filter #((complement contains?) (get-in r [% :actions]) action) apps)]
-          [action filtered-refs])))))
-    
+    (for [[action apps] ref-map]
+      (let [filtered-refs (remove #(contains? (get-actions r %) action) apps)]
+        [action filtered-refs]))))
+
 (defn filter-included-actions [r]
   "Retain only apps that call a intent that is not defined within their own
    manifest.xml."
@@ -20,9 +22,37 @@
     
 (def android-apps (deserialize "results/potential-refs"))
 
-(def real-external-refs (filter 
-         (fn [[_ {v :references-from}]] (not-empty v)) 
-         (filter-included-actions android-apps)))
+(def real-external-refs 
+  (filter 
+    (fn [[_ {v :references-from}]] (not-empty v)) 
+    (filter-included-actions android-apps)))
   
 (printf "Got %d real references between apps.\n" (count real-external-refs))
-(pprint real-external-refs)
+(binding [*print-length* 10]
+  (pprint real-external-refs))
+
+(comment
+  (use 'clojure.contrib.json)
+  (spit "results/real-refs" (with-out-str (pprint-json real-external-refs))))
+
+(defn valid-action? [s]
+  (< 1 (count (filter #(= % \.)  s))))
+
+(defn get-ref-strings [foo]
+  (for [[app-name {refs :references-from}] foo]
+    (for [[action apps] refs :when (valid-action? action)]
+      (for [a apps]
+        (str \" a "\"->\"" action "\"\n")))))
+
+(defn graphviz [foo]
+  (str 
+    "digraph {\n"
+    ;; app to action strings
+    (apply str (interpose " "
+                 (for [[k v] foo] 
+                   (str "\"" k "\"->{"
+                     (apply str (for [a (:actions v)]
+                                  (str "\"" a "\"")))
+                     "} \n"))))
+    (apply str (interpose " " (flatten (get-ref-strings foo))))
+    "}"))
