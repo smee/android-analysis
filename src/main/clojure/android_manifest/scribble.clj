dba5805efb2b95aec788a87818615a757a0476f0
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2010-10-20 13:36:15 +0000

    enhanced visulization
    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@368 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- src/main/clojure/android_manifest/scribble.clj
+++ src/main/clojure/android_manifest/scribble.clj
@@ -6,20 +6,60 @@
   (get-in all-apps [app-name :actions]))
 
 (defn filter-references [ref-map r]
-  "Retain only apps that call a intent that is not defined within their own
+  "Retain only apps that call an intent that is not defined within their own
    manifest.xml."
   (into {}
-    (for [[action apps] ref-map]
-      (let [filtered-refs (remove #(contains? (get-actions r %) action) apps)]
+    (for [[action dependent-apps] ref-map]
+      (let [filtered-refs (remove #(contains? (get-actions r %) action) dependent-apps)]
         [action filtered-refs]))))
 
 (defn filter-included-actions [r]
-  "Retain only apps that call a intent that is not defined within their own
+  "Retain only apps that call an intent that is not defined within their own
    manifest.xml."
   (into {}
    (for [[app-name {refs :references-from :as m}] r]
      [app-name (assoc m :references-from (filter-references refs r))])))
     
+
+(defn valid-action? [s]
+  (< 1 (count (filter #(= % \.)  s))))
+
+
+(defn get-ref-strings [all-apps]
+  (distinct (flatten
+    (for [[app-name {refs :references-from}] all-apps]
+      (for [[action apps] refs :when (valid-action? action)]
+        (for [app apps]
+          (str
+            \" app \" "[shape=box] \n"
+            \" app \" "->" \" action \" " [color=red] ;\n")))))))
+
+
+(defn get-action-def-strings [all-apps]
+  (let [action-defs (for [[app-name attr-map] all-apps]
+                      [app-name 
+                       (for [[action dependent-apps] (:references-from attr-map) :when (not-empty dependent-apps)]
+                         action)])]
+    
+    (for [[name actions] action-defs :when (not-empty actions)]
+      (str
+        \" name \" "[shape=box] ; \n"
+        \" name \" "->{"
+        (apply str (for [action actions]
+                     (str "\"" action "\" ")))
+        "} [color=green] ; \n"))))
+           
+
+(defn graphviz [real-external-refs]
+  (str 
+    "digraph {\n"
+    ;; app to action strings
+    (apply str (interpose " " (get-action-def-strings real-external-refs)))
+    ;; dependent apps to action strings
+    (apply str (interpose " " (get-ref-strings real-external-refs)))
+    "}"))
+
+;; script start
 (def android-apps (deserialize "results/potential-refs"))
 
 (def real-external-refs 
@@ -29,30 +69,14 @@
   
 (printf "Got %d real references between apps.\n" (count real-external-refs))
 (binding [*print-length* 10]
-  (pprint real-external-refs))
+  (pprint (take 10 real-external-refs)))
 
 (comment
   (use 'clojure.contrib.json)
-  (spit "results/real-refs" (with-out-str (pprint-json real-external-refs))))
-
-(defn valid-action? [s]
-  (< 1 (count (filter #(= % \.)  s))))
-
-(defn get-ref-strings [foo]
-  (for [[app-name {refs :references-from}] foo]
-    (for [[action apps] refs :when (valid-action? action)]
-      (for [a apps]
-        (str \" a "\"->\"" action "\"\n")))))
-
-(defn graphviz [foo]
-  (str 
-    "digraph {\n"
-    ;; app to action strings
-    (apply str (interpose " "
-                 (for [[k v] foo] 
-                   (str "\"" k "\"->{"
-                     (apply str (for [a (:actions v)]
-                                  (str "\"" a "\"")))
-                     "} \n"))))
-    (apply str (interpose " " (flatten (get-ref-strings foo))))
-    "}"))
+  ;; write output as json file
+  (spit "results/real-refs" (with-out-str (pprint-json real-external-refs)))
+  ;; visualize results via graphviz
+  (spit "results/real-external-refs.dot" (graphviz real-external-refs))
+  )
+;; total number of referenced intents?
+;; # of openintents?
