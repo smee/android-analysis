6dd4f6197ae88241f32dfb8194497ab5935a06fb
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2010-10-20 14:35:10 +0000

    reduced code, introduced record instead of anonymous map for our data
    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@369 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- src/main/clojure/android_manifest/core.clj
+++ src/main/clojure/android_manifest/core.clj
@@ -5,9 +5,8 @@
         android-manifest.serialization)
   (:import java.io.File))
 
-(defn files-in [dir]
-  (for [file (seq (.listFiles (File. dir)))]
-    file))
+
+(defrecord Android-App [name version package actions references-from])
 
 (defn find-file [dirpath pattern]
   "Traverse directory dirpath depth first, return all files matching
@@ -28,36 +27,37 @@ the regular expression pattern"
 
 
 
-(defn create-non-android-action-map [xml]
-  (let [package-name        (-> xml first :attrs :package)
+(defn load-android-app [manifest-file]
+  (let [xml                 (xml-seq (xml/parse manifest-file))
+        app-name            (-> manifest-file .getParentFile .getName)
+        package-name        (-> xml first :attrs :package)
         version             (-> xml first :attrs :android:versionName)
         all-actions         (get-actions xml)
-        non-android-actions (filter #(and 
-                                       (not (.startsWith % "android.")) 
-                                       (not (.startsWith % "com.android."))
-                                       (not (.startsWith % "com.google.android."))) all-actions)]
-    (hash-map 
-      :package package-name
-      :version version
-      :actions (into #{} non-android-actions))))
+        non-android-actions (into #{} (filter #(and 
+                                                 (not (.startsWith % "android.")) 
+                                                 (not (.startsWith % "com.android."))
+                                                 (not (.startsWith % "com.google.android."))) all-actions))]
+    (Android-App. 
+      app-name 
+      version 
+      package-name
+      non-android-actions
+      {})))
 
  (defn extract-path-part [idx path]
    (let [paths (.split path "\\\\")
          app-name (nth paths idx)]
      app-name))
  
-(defn shorten-path [{r :references-from p :path :as m}] 
-   "Extract application name from references pathes."
-   (assoc m
-     ;; my path is d:/android/decompiled/site-name/app-name
-     :path (extract-path-part 4 p)))
-     
+ 
  
 (defn search-action-references [actions lucene-index-dir]
+  "Create map of action names to a set of app names that reference this action."
   (into {}
     (for [a actions]
-      [a (distinct 
+      [a (into #{} 
            (map 
+             ;; my index was created on a directory hierarchy where the app was at depth 8
              (partial extract-path-part 8) 
              (search-lucene lucene-index-dir a)))])))
 
@@ -67,11 +67,9 @@ the regular expression pattern"
    Returns a map with keys :package (package name of the manifest), :actions (non-android action strings) and
    :references-from (paths of smali files containing any of these actions)."
   (let
-    [actions-map           (map #(create-non-android-action-map (xml-seq (xml/parse %))) manifest-files)
-     path-actions-map      (map #(assoc %2 :path (.getParent %1)) manifest-files actions-map)]
-    (for [{actions :actions :as m} path-actions-map]
-      (shorten-path
-        (assoc m :references-from (search-action-references actions lucene-index-dir))))))
+    [android-apps (map load-android-app manifest-files)]
+    (for [{actions :actions :as m} android-apps]
+      (assoc m :references-from (search-action-references actions lucene-index-dir)))))
 
   
 (defn change-values [m f]
@@ -84,26 +82,25 @@ the regular expression pattern"
  (defn foreign-refs-only [maps]
    "Remove all potentially external action references, if they came from
     the application itself."
-   (for [{r :references-from p :path :as m} maps]
-     (assoc m :references-from (remove-empty-values (change-values r (partial filter #(not= % p)))))))
+   (for [{refs :references-from name :name :as m} maps]
+     (assoc m :references-from 
+       (remove-empty-values 
+         (change-values refs (partial filter #(not= % name)))))))
  
  
  
 (comment
    
-  (find-all-references "d:/android/decompiled" "d:/android/lucene-index")
-
-;; or
-(set! *print-length* 15)
- (def app-sources "D:/android/decompiled")
- (def index-dir "D:/android/lucene-index")
+  (set! *print-length* 15)
+  (def app-sources "D:/android/decompiled")
+  (def index-dir "D:/android/lucene-index")
  
- ;; create sequence of maps with :path, :actions, :references-from
+  ;; create sequence of maps with :path, :actions, :references-from
  (def r (foreign-refs-only         
-            (find-all-references (find-file android-source-dir #".*AndroidManifest.xml") index-dir)))
+          (find-all-references (find-file app-sources #".*AndroidManifest.xml") index-dir)))
  ;; or
  (def r (foreign-refs-only         
-            (find-all-references (map #(File. %) (deserialize "d:/android/all-manifests")) index-dir)))
- 
+          (find-all-references (map #(File. %) (deserialize "d:/android/all-manifests")) index-dir)))
+ (use 'clojure.pprint)
  
 )
