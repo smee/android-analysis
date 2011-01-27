3938359af5c08ab955875faf331db4eecdc96ce4
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2011-01-27 14:49:37 +0000

    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@494 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- src/main/clojure/android/analysis/manifest.clj
+++ src/main/clojure/android/analysis/manifest.clj
@@ -101,6 +101,21 @@ in loading android apps without duplicates (same package, lower versions)."
   [dir]
   (find-file dir #".*AndroidManifest.xml"))
 
+;;;; Parsing via xmlpullparser
+(defn- attrs [xpp]
+  (for [i (range (.getAttributeCount xpp))]
+    [(keyword (.getAttributeName xpp i))
+     (.getAttributeValue xpp i)]))
+
+(defn- ns-decs [xpp]
+  (let [d (.getDepth xpp)]
+    (for [i (range (.getNamespaceCount xpp (dec d)) (.getNamespaceCount xpp d))]
+      (let [prefix (.getNamespacePrefix xpp i)]
+        [(keyword (str "xmlns" (when prefix (str ":" prefix))))
+         (.getNamespaceUri xpp i)]))))
+
+(defn- attr-hash [xpp]
+  (into {} (concat (ns-decs xpp) (attrs xpp))))
 
 (defn- pull-step 
   "lazy sequence of tag names."
@@ -108,31 +123,28 @@ in loading android apps without duplicates (same package, lower versions)."
   (let [step (fn [xpp]
                (condp = (.next xpp)
                  XmlPullParser/START_TAG
-                 (cons {(keyword (.getName xpp)) (.getDepth xpp)}
+                 (cons {:tag (keyword (.getName xpp)) :depth (.getDepth xpp) :attr (attr-hash xpp)}
                    (pull-step xpp))
                  XmlPullParser/END_TAG
-                 (cons {(keyword (str "/" (.getName xpp))) (.getDepth xpp)}
+                 (cons {:tag (keyword (str "/" (.getName xpp))) :depth (.getDepth xpp)}
                    (pull-step xpp))
                  XmlPullParser/TEXT
-                 (let [text (.trim (.getText xpp))]
-                   (if (empty? text)
-                     (recur xpp)
-                     (cons {text (.getDepth xpp)}
-                       (pull-step xpp))))
+                 (cons nil (pull-step xpp))
                  nil))]
-    (lazy-seq (step xpp))))
+    (remove nil? (lazy-seq (step xpp)))))
 
 (defn- init-parser [filename]
-  filename)
+  (doto (org.kxml2.io.KXmlParser.) 
+    (.setFeature XmlPullParser/FEATURE_PROCESS_NAMESPACES true)
+    (.setInput (clojure.java.io/reader filename))))
 
 (defn create-intent-filters 
   "Extract all intent-filter definitions and create instances of android.IntentFilter.
 Use it's .match method to decide whether an intent matches a filter."
   [filename]
   (let [parser (init-parser filename)
-        tags   (pull-step parser)]
-    
-    (for [i-f (filter #(= :intent-filter %) tags)]
+        tags   (pull-step parser)]    
+    (for [i-f (filter #(= :intent-filter (:tag %)) tags)]
       (doto (android.IntentFilter.) 
         (.readFromXml parser)))))
 
