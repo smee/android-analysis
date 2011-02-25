48eba62b675db0f1f66895b117bf4eb7e3a96c46
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2011-02-25 11:39:47 +0000

    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@531 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject android-manifest "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dev-dependencies  [[vimclojure/server "2.2.0-SNAPSHOT"]
-                      [marginalia "0.2.0"]] 
+                      [marginalia "0.3.2"]] 
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.lucene/lucene-core "3.0.2"]]
