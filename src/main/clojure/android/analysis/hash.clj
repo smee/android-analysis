d51ec8e4e651100ec78f84a42981b8f06efa36a6
steffend <steffend@a697ffe5-49c8-4aff-8373-c6129aaec4cf>
2011-11-04 16:25:09 +0000

    [refactor] move library identification code
    git-svn-id: https://gdansk.uwaterloo.ca/svn/berger/android@680 a697ffe5-49c8-4aff-8373-c6129aaec4cf

--- src/main/clojure/android/analysis/hash.clj
+++ src/main/clojure/android/analysis/hash.clj
@@ -18,3 +18,50 @@
 
 (defn md5-of [obj]
   (md5 (.getBytes (print-str obj))))
+
+
+(defn package-of-class 
+  "extract package of class file name , e.g. (package-of-class \"a/b/c/d.class\") returns
+\"a/b/c\""
+  [clz]
+  (let [idx (.lastIndexOf clz "/")]
+    (if (not= -1 idx) (subs clz 0 idx) "")))
+
+(defn hashes-per-package 
+  "Takes map of class file names to md5 hashes, returns map of packages to set of md5
+hashes of the files in that package."
+  [hashes]
+  (let [hpp (->> hashes
+              reverse-map
+              (group-by (comp package-of-class second))
+              (map-values (comp (partial into (sorted-set)) (partial map first))))]
+    hpp))
+
+(defn not-distinct
+  "Returns a lazy sequence of the elements of coll that have duplicates"
+  {:added "1.0"}
+  [coll]
+    (let [step (fn step [xs seen]
+                   (lazy-seq
+                    ((fn [[f :as xs] seen]
+                      (when-let [s (seq xs)]
+                        (if ((complement contains?) seen f) 
+                          (recur (rest s) (conj seen f))
+                          (cons f (step (rest s) seen)))))
+                     xs seen)))]
+      (distinct (step coll #{}))))
+
+(defn android-libraries [hashes]
+  (let [hpp (pmap #(->> % second hashes-per-package (map-values hash/md5-of)) hashes)
+        duplicates (not-distinct (apply concat hpp))]
+    duplicates))
+
+
+(comment
+  
+    (let [hashes (pmap #(list (.getName %) (deserialize %)) (find-files "z:/classes-md5/" #".*\d{4}\d*"))
+        libs (android-libraries hashes)
+        ]
+    (serialize "z:/reduced/identified-libs" libs)
+    )
+    )
